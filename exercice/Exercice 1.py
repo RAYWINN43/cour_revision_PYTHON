00bdfc9959a9dac0fc5d14b7357055960630415e
Antoine <antoineblain43@gmail.com>
2025-12-01 14:19:38 +0100

    exercice

---
+++ exercice/Exercice 1.py
@@ -0,0 +1,8 @@
+# Écrivez une fonction Python qui prend une séquence de nombres et détermine si tous les nombres sont différents les uns des autres.
+
+test_distinct[1, 5, 7, 9]
+test_distinct[2, 4, 5, 5, 7, 9]
+
+a=len(test_distinct)
+for i in  a:
+    print(a)
