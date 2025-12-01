e3f10318d2a1036c31233a64885064b12ea8aebc
Antoine <antoineblain43@gmail.com>
2025-12-01 14:19:40 +0100

    update exercice

--- exercice/Exercice 1.py
+++ exercice/Exercice 1.py
@@ -1,8 +1,12 @@
 # Écrivez une fonction Python qui prend une séquence de nombres et détermine si tous les nombres sont différents les uns des autres.
+# test_distinct=[1, 5, 7, 9]
+# test_distinct=[2, 4, 5, 5, 7, 9]
 
-test_distinct[1, 5, 7, 9]
-test_distinct[2, 4, 5, 5, 7, 9]
-
-a=len(test_distinct)
-for i in  a:
-    print(a)
+def distinctlist(test_distinct):
+    if len(test_distinct) == len(set(test_distinct)):
+        return True   
+    else:
+        return False  
+    
+print(distinctlist([1, 5, 7, 9])) 
+print(distinctlist([2, 4, 5, 5, 7, 9])) 
---
+++ exercice/Exercice 2.py
@@ -0,0 +1,15 @@
+# Écrivez un programme Python pour trouver la médiane parmi trois nombres donnés comme input
+
+
+# Input the first number 25
+# Input the second number 15
+# Input the third number 35
+# Median of the above three numbers -
+# 25
+
+a=25
+b=15
+c=35
+
+import statistics
+print(statistics.median([a,b,c]))
